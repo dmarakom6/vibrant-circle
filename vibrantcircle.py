d7f65b8e3b56638a3494d2a33fe5e091c7a44ad9
dmarakom6 <dmarakom@gmail.com>
2021-02-18 08:58:20 +0200

    Better code in loop

--- vibrantcircle.py
+++ vibrantcircle.py
@@ -14,12 +14,11 @@ t.penup()
 t.goto(0, 200)
 t.pendown()
 
-while True:
+while b != 210:
     t.forward(a)
     t.right(b)
     a += 3
     b+= 1
-    if b == 210:
-        break
     t.hideturtle()
+
 turtle.done()
